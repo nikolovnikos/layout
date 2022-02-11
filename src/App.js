babfb0ccb15a0a584ce67b8e7211bccd318d3077
nikolay.v.nikolov@mansion.com <nikolay.v.nikolov@mansion.com>
2022-02-11 22:24:13 +0100

    add layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,86 @@
+import {
+  useState,
+  useEffect,
+  useMemo,
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LayoutZeplin, devicesDimensions } from './helpers/LayoutZeplinConverter';
+
+const layoutZMobile = new LayoutZeplin(devicesDimensions.iphone11_1111);
+const layoutZTablet = new LayoutZeplin(devicesDimensions.ipad_1111);
 
 function App() {
+
+  const getCurrentOrientation = () => {
+    console.log('1');
+    return window.innerWidth < window.innerHeight ? 'portrait' : 'landscape';
+  }
+
+  const [orientation, setOrienation] = useState(() => getCurrentOrientation());
+
+
+	useEffect(() => {
+    function handleResize() {
+      setOrienation(() => getCurrentOrientation());
+    }
+    window.addEventListener('resize', handleResize)
+		return function cleanup() {
+			window.removeEventListener('resize', handleResize)
+		};
+  }, []);
+  
+  const dimenstions = useMemo(() => {
+    console.log(orientation);
+    let marginTopTemplate = 0;
+    let marginBottom = 0;
+    let marginTopGameRow = 0;
+    let marginLeftGameRow = 0;
+    let fontSizeHeaderTemplate = 0;
+    let marginBottomHeaderTemplate = 0;
+    let paddingHorizontalTemplate = 0;
+    let podXL = { width: 0, height: 0 };
+    let podM = { width: 0, height: 0 };
+    let podS = { width: 0, height: 0 };
+    // // if (Device.isTablet()) {
+    //   marginTopTemplate = layoutZTablet.getHeight([15, 15], orientation);
+    //   marginBottom = layoutZTablet.getHeight([31, 31], orientation);
+    //   marginTopGameRow = layoutZTablet.getHeight([8, 8], orientation);
+    //   marginLeftGameRow = layoutZTablet.getHeight([8, 8], orientation);
+    //   fontSizeHeaderTemplate = layoutZTablet.getFontSize([18, 18], orientation);
+    //   marginBottomHeaderTemplate = layoutZTablet.getHeight([15, 15], orientation);
+    //   paddingHorizontalTemplate = layoutZTablet.getWidth([8, 6], orientation);
+    //   podXL = layoutZTablet.getBox([372, 332], [372, 332], orientation);
+    //   podM = layoutZTablet.getBox([372, 332], [182, 162], orientation);
+    //   podS = layoutZTablet.getBox([182, 162], [182, 162], orientation);
+    // } else {
+      marginTopTemplate = layoutZMobile.getHeight([15, 15], orientation);
+      marginBottom = layoutZMobile.getHeight([31, 31], orientation);
+      marginTopGameRow = layoutZMobile.getHeight([8, 8], orientation);
+      marginLeftGameRow = layoutZMobile.getHeight([8, 8], orientation);
+      fontSizeHeaderTemplate = layoutZMobile.getFontSize([18, 18], orientation);
+      marginBottomHeaderTemplate = layoutZMobile.getHeight([15, 15], orientation);
+      paddingHorizontalTemplate = layoutZMobile.getWidth([8, 8], orientation);
+      podXL = layoutZMobile.getBox([398, 388], [398, 388], orientation);
+      podM = layoutZMobile.getBox([398, 388], [195, 190], orientation);
+      podS = layoutZMobile.getBox([195, 190], [195, 190], orientation);
+    // }
+
+    const dimensions = {
+      marginTopTemplate,
+      marginBottom,
+      marginTopGameRow,
+      marginLeftGameRow,
+      fontSizeHeaderTemplate,
+      marginBottomHeaderTemplate,
+      podXL,
+      podM,
+      podS,
+      paddingHorizontalTemplate,
+    };
+    return dimensions;
+  }, [orientation]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +96,9 @@ function App() {
         >
           Learn React
         </a>
+        <div style={{ backgroundColor: 'red', width: dimenstions.podXL.width, height:  dimenstions.podXL.height }}>
+          Test Difff
+        </div>
       </header>
     </div>
   );
---
+++ src/helpers/LayoutZeplinConverter.js
@@ -0,0 +1,104 @@
+// import {
+//   Dimensions,
+//  } from 'react-native';
+// import { storeOrientation } from '../stores/store';
+
+// import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+/*
+  Array keys of the device
+  0 - deviceWidth,
+  1 - deviceHeight,
+  2 - deviceWidthLand,
+  3 - deviceHeightLand
+*/
+const devicesDimensions = {
+  iphone11_1111: [414, 814, 800, 393],
+  ipad_1111: [768, 1024, 1024, 768],
+  // iphone 11 with top, bottom, left, right safearea
+  // {"bottom": 34, "left": 0, "right": 0, "top": 48} - Portrait
+  // {"bottom": 21, "left": 48, "right": 48, "top": 0} - Landscape
+};
+
+class LayoutZeplin {
+  #deviceWidth;
+
+  #deviceHeight;
+
+  #deviceWidthLand;
+
+  #deviceHeightLand;
+
+  constructor(device = [0, 0, 0, 0]) {
+    const [
+      deviceWidth,
+      deviceHeight,
+      deviceWidthLand,
+      deviceHeightLand,
+     ] = device;
+    this.#deviceWidth = deviceWidth;
+    this.#deviceHeight = deviceHeight;
+    this.#deviceWidthLand = deviceWidthLand;
+    this.#deviceHeightLand = deviceHeightLand;
+  }
+
+  getWidth = (boxWidthArrayZ = [0, 0], orientation = 'portrait') => {
+    // const insets = useSafeAreaInsets();
+    // const { left, right } = storeOrientation.getState().safeAreaInsets;
+    // console.log(storeOrientation.getState().safeAreaInsets);
+    const windowWidth = window.innerWidth;
+    let boxWidth = boxWidthArrayZ[0];
+    let windowWidthZeplin = this.#deviceWidth;
+    if (orientation === 'landscape') {
+      windowWidthZeplin = this.#deviceWidthLand;
+      boxWidth = boxWidthArrayZ[1];
+      if (boxWidth > windowWidthZeplin) {
+        boxWidth = windowWidthZeplin;
+      }
+    }
+    // console.log(insets);
+    const ratio = windowWidth / windowWidthZeplin;
+    return boxWidth * ratio;
+  }
+
+  getHeight = (boxHeightArrayZ = [0, 0], orientation = 'portrait') => {
+    // const insets = useSafeAreaInsets();
+    // console.log(bottom, top);
+    const windowHeight = window.innerHeight;
+    // console.log(windowHeight);
+    let boxHeight = boxHeightArrayZ[0];
+    let windowHeightZeplin = this.#deviceHeight;
+    if (orientation === 'landscape') {
+      windowHeightZeplin = this.#deviceHeightLand;
+      boxHeight = boxHeightArrayZ[1];
+      if (boxHeight > windowHeightZeplin) {
+        boxHeight = windowHeightZeplin;
+      }
+    }
+    // console.log(insets);
+    const ratio = windowHeight / windowHeightZeplin;
+    return boxHeight * ratio;
+  }
+
+  /*
+    boxWidthArrayZ = [ widthPortrait, widthLanscape ]
+    boxHeightArrayZ = [ ]
+  */
+  getBox = (boxWidthArrayZ = [0, 0], boxHeightArrayZ = [0, 0], orientation) => {
+    let ratioZ = boxWidthArrayZ[0] / boxHeightArrayZ[0];
+    if (orientation === 'landscape') {
+      ratioZ = boxWidthArrayZ[1] / boxHeightArrayZ[1];
+    }
+    const w = this.getWidth(boxWidthArrayZ, orientation);
+    const h = w / ratioZ;
+    // console.log(ratioZ, w, h);
+    return { width: w, height: h };
+  }
+
+  getFontSize = (fontSizeArrayZ = [0, 0], orientation = 'portrait') => {
+    const dimentios = this.getBox(fontSizeArrayZ, fontSizeArrayZ, orientation);
+    return dimentios.height;
+  }
+}
+
+export { devicesDimensions, LayoutZeplin };
