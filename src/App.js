a5fd3933161f8a69f79fc1da587b34e21ba7b165
nikolay.v.nikolov@mansion.com <nikolay.v.nikolov@mansion.com>
2022-02-12 12:13:44 +0100

    add layout screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import {
   useState,
   useEffect,
-  useMemo,
+  useRef,
 } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { LayoutZeplin, devicesDimensions } from './helpers/LayoutZeplinConverter';
+import LayoutScreenConverter from './components/layoutScreen';
 
-const layoutZMobile = new LayoutZeplin(devicesDimensions.iphone11_1111);
-const layoutZTablet = new LayoutZeplin(devicesDimensions.ipad_1111);
 
 const deviceType = () => {
+  console.log(navigator.userAgent)
   const ua = navigator.userAgent;
   if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
       return "tablet";
@@ -18,13 +16,21 @@ const deviceType = () => {
   else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
       return "mobile";
   }
+  if (window.innerWidth <= 1080) {
+    return "tablet";
+  }
   return "desktop";
 };
 
 function App() {
 
+  const device = useRef(deviceType());
+
   const getCurrentOrientation = () => {
-    console.log('1');
+    // console.log('1');
+    if (device.current === 'desktop') {
+      return 'landscape';
+    }
     return window.innerWidth < window.innerHeight ? 'portrait' : 'landscape';
   }
 
@@ -40,78 +46,10 @@ function App() {
 			window.removeEventListener('resize', handleResize)
 		};
   }, []);
-  
-  const dimenstions = useMemo(() => {
-    console.log(orientation);
-    let marginTopTemplate = 0;
-    let marginBottom = 0;
-    let marginTopGameRow = 0;
-    let marginLeftGameRow = 0;
-    let fontSizeHeaderTemplate = 0;
-    let marginBottomHeaderTemplate = 0;
-    let paddingHorizontalTemplate = 0;
-    let podXL = { width: 0, height: 0 };
-    let podM = { width: 0, height: 0 };
-    let podS = { width: 0, height: 0 };
-    console.log(deviceType());
-    if (deviceType() === 'tablet') {
-      marginTopTemplate = layoutZTablet.getHeight([15, 15], orientation);
-      marginBottom = layoutZTablet.getHeight([31, 31], orientation);
-      marginTopGameRow = layoutZTablet.getHeight([8, 8], orientation);
-      marginLeftGameRow = layoutZTablet.getHeight([8, 8], orientation);
-      fontSizeHeaderTemplate = layoutZTablet.getFontSize([18, 18], orientation);
-      marginBottomHeaderTemplate = layoutZTablet.getHeight([15, 15], orientation);
-      paddingHorizontalTemplate = layoutZTablet.getWidth([8, 6], orientation);
-      podXL = layoutZTablet.getBox([372, 332], [372, 332], orientation);
-      podM = layoutZTablet.getBox([372, 332], [182, 162], orientation);
-      podS = layoutZTablet.getBox([182, 162], [182, 162], orientation);
-    } else if (deviceType() === 'mobile') {
-      marginTopTemplate = layoutZMobile.getHeight([15, 15], orientation);
-      marginBottom = layoutZMobile.getHeight([31, 31], orientation);
-      marginTopGameRow = layoutZMobile.getHeight([8, 8], orientation);
-      marginLeftGameRow = layoutZMobile.getHeight([8, 8], orientation);
-      fontSizeHeaderTemplate = layoutZMobile.getFontSize([18, 18], orientation);
-      marginBottomHeaderTemplate = layoutZMobile.getHeight([15, 15], orientation);
-      paddingHorizontalTemplate = layoutZMobile.getWidth([8, 8], orientation);
-      podXL = layoutZMobile.getBox([398, 388], [398, 388], orientation);
-      podM = layoutZMobile.getBox([398, 388], [195, 190], orientation);
-      podS = layoutZMobile.getBox([195, 190], [195, 190], orientation);
-    }
-
-    const dimensions = {
-      marginTopTemplate,
-      marginBottom,
-      marginTopGameRow,
-      marginLeftGameRow,
-      fontSizeHeaderTemplate,
-      marginBottomHeaderTemplate,
-      podXL,
-      podM,
-      podS,
-      paddingHorizontalTemplate,
-    };
-    return dimensions;
-  }, [orientation]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div style={{ backgroundColor: 'red', width: dimenstions.podXL.width, height:  dimenstions.podXL.height }}>
-          Test Difff
-        </div>
-      </header>
+      <LayoutScreenConverter orientation={orientation}/>
     </div>
   );
 }
---
+++ src/components/layoutScreen.js
@@ -0,0 +1,295 @@
+import {
+  useState,
+  useEffect,
+  useMemo,
+} from 'react';
+// import {
+//  View,
+//  StyleSheet,
+//  Text,
+// } from 'react-native';
+
+import PropTypes from 'prop-types';
+
+// import {
+//   storeOrientation,
+// } from '../../stores/store';
+// import { STYLES } from '../../styles/common';
+
+import { LayoutZeplin, devicesDimensions } from '../helpers/LayoutZeplinConverter';
+/*
+  Zeplin layout source:
+  https://app.zeplin.io/project/61a8a9480bf3cf8df5b66ab2/screen/61b70b84075a1e48410f5f88
+*/
+const layoutZPhone = new LayoutZeplin(devicesDimensions.iphone11_1111);
+
+const LayoutScreenConverter = ({
+  orientation = 'portrait',
+}) => {
+  const rectangleGreen = () => {
+    console.log(orientation);
+    const widthsPhone = [414, 800];
+    const heightsPhone = [48, 48];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        backgroundColor: '#49c400',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      />
+    );
+  };
+
+  const rectangleOrange = () => {
+    const widthsPhone = [414, 800];
+    const heightsPhone = [48, 48];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        backgroundColor: '#ff9c24',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      />
+    );
+  };
+
+  const rectangleWhite = () => {
+    const widthsPhone = [414, 800];
+    const heightsPhone = [80, 80];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        backgroundColor: '#ffffff',
+        display: 'table',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      >
+        <div style={{
+          fontSize: layoutZPhone.getFontSize([32, 32], orientation),
+          color: 'black',
+          display: 'table-cell',
+          verticalAlign: 'middle',
+          fontFamily: 'Gotham-Bold',
+        }}
+        >
+          Type something here AW
+        </div>
+      </div>
+    );
+  };
+
+  const rectangleBigBlack = () => {
+    const widthsPhone = [414, 800];
+    const heightsPhone = [162, 162];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        backgroundColor: '#000',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      />
+    );
+  };
+
+  const rectanglesGray = () => {
+    const widthsPhone = [207, 207];
+    const heightsPhone = [96, 96];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (<div style={{
+      width: layoutZPhone.getWidth([414, 800], orientation),
+      marginTop: layoutZPhone.getHeight([48, 48], orientation),
+      height: dimentios.height,
+      // justifyContent: 'space-between',
+      // flexDirection: 'row',
+      position: 'relative'
+    }}
+    >
+      {rectangleGray1()}
+      {rectangleGray2()}
+    </div>)
+  };
+
+  const rectangleGray1 = () => {
+    const widthsPhone = [207, 207];
+    const heightsPhone = [96, 96];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: '100%',
+        backgroundColor: '#959595',
+        float: 'left',
+      }}
+      >
+        <div style={{
+          fontSize: layoutZPhone.getFontSize([12, 12], orientation),
+          marginLeft: layoutZPhone.getWidth([12, 12], orientation),
+          marginTop: layoutZPhone.getHeight([12, 12], orientation),
+          color: '#ffffff',
+          position: 'absolute',
+          fontFamily: 'Gotham-Light',
+        }}
+        >
+          Type something here ABSDAWR
+        </div>
+      </div>
+    );
+  };
+
+  const rectangleGray2 = () => {
+    const widthsPhone = [110, 110];
+    const heightsPhone = [96, 96];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: '100%',
+        backgroundColor: '#cccccc',
+        float: 'right',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      />
+    );
+  };
+
+  const rectangleDarkGray = () => {
+    const widthsPhone = [222, 222];
+    const heightsPhone = [72, 72];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    const marginTop = layoutZPhone.getHeight([84, 84], orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        marginTop,
+        marginLeft: layoutZPhone.getWidth([96, 96], orientation),
+        backgroundColor: '#383838',
+        position: 'relative',
+        display: 'table',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      >
+        <div style={{
+          fontSize: layoutZPhone.getFontSize([16, 80], orientation),
+          color: '#ffffff',
+          display: 'table-cell',
+          verticalAlign: 'middle',
+          fontFamily: 'Gotham-Medium',
+        }}
+        >
+          Type something
+        </div>
+      </div>
+    );
+  };
+
+  const rectanglesColor = () => {
+    return (<div style={{
+      width: layoutZPhone.getWidth([414, 800], orientation),
+      marginTop: layoutZPhone.getHeight([56, 56], orientation),
+      paddingTop: layoutZPhone.getHeight([8, 8], orientation),
+      paddingBottom: layoutZPhone.getHeight([8, 8], orientation),
+      paddingLeft: layoutZPhone.getWidth([10, 10], orientation),
+      paddingRight: layoutZPhone.getWidth([10, 10], orientation),
+      position: 'relative',
+      // flexDirection: 'column',
+      display: 'flex',
+      // justifyContent: 'space-between',
+      backgroundColor: '#afafaf',
+    }}
+    >
+      {rectangleCian()}
+      {rectangleBlue()}
+      {rectanglePing()}
+    </div>);
+  };
+
+  const rectangleCian = () => {
+    const widthsPhone = [124, 124];
+    const heightsPhone = [104, 104];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        backgroundColor: '#55ecce',
+        // float: 'left',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      />
+    );
+  };
+
+  const rectangleBlue = () => {
+    const widthsPhone = [159, 159];
+    const heightsPhone = [104, 104];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        backgroundColor: '#556eec',
+        marginLeft: layoutZPhone.getWidth([12, 12], orientation),
+        marginRight: layoutZPhone.getWidth([12, 12], orientation),
+        // float: 'left',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      />
+    );
+  };
+
+  const rectanglePing = () => {
+    const widthsPhone = [87, 87];
+    const heightsPhone = [104, 104];
+    const dimentios = layoutZPhone.getBox(widthsPhone, heightsPhone, orientation);
+    return (
+      <div style={{
+        width: dimentios.width,
+        height: dimentios.height,
+        backgroundColor: '#cc55ec',
+        // float: 'left',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+      />
+    );
+  };
+
+  return (
+    <div style={{
+      backgroundColor: 'white',
+      flexDirection: 'column',
+    }}>
+      {rectangleGreen()}
+      {rectangleOrange()}
+      {rectangleWhite()}
+      {rectangleBigBlack()}
+      {rectanglesGray()}
+      {rectangleDarkGray()}
+      {rectanglesColor()}
+    </div>
+  );
+};
+
+
+LayoutScreenConverter.propTypes = {
+  orientation: PropTypes.string.isRequired,
+};
+
+export default LayoutScreenConverter;
