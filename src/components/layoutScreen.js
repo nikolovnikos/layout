88951f24cda61998135784ce122bce4b7f5faa65
nikolay.v.nikolov@mansion.com <nikolay.v.nikolov@mansion.com>
2022-02-15 16:48:54 +0200

    optimize

--- src/components/layoutScreen.js
+++ src/components/layoutScreen.js
@@ -3,237 +3,42 @@ import {
 } from 'react';
 
 import PropTypes from 'prop-types';
-import { LayoutZeplin, devicesDimensions } from '../helpers/LayoutZeplinConverter';
-/*
-  Mobile
-  https://app.zeplin.io/project/61a8a9480bf3cf8df5b66ab2/screen/61b70b84075a1e48410f5f88
-*/
-const layoutZPhone = new LayoutZeplin(devicesDimensions.iphone11_1111);
-/*
-  Desktop
-  https://app.zeplin.io/project/61a8a9480bf3cf8df5b66ab2/screen/620a368451206fa8e5757617
-*/
-const layoutZDesktop = new LayoutZeplin(devicesDimensions.desktop_1440);
-
-/*
-  Tablet
-  https://app.zeplin.io/project/61a8a9480bf3cf8df5b66ab2/screen/620a3678a46278aabb5de991
-*/
-const layoutZTablet = new LayoutZeplin(devicesDimensions.ipad_1111);
-
-const stylesZ = {
-  rectangleGreen: {
-    width: 0,
-    height: 0,
-  },
-  rectangleOrange: {
-    width: 0,
-    height: 0,
-  },
-  rectangleWhite: {
-    width: 0,
-    height: 0,
-    fontSize: 0,
-  },
-  rectangleBigBlack: {
-    width: 0,
-    height: 0,
-  },
-  rectanglesGray: {
-    width: 0,
-    height: 0,
-    marginTop: 0,
-    marginLeft: 0,
-  },
-  rectangleDarkGray: {
-    width: 0,
-    height: 0,
-    marginLeft: 0,
-    marginTop: 0,
-    fontSize: 0,
-  },
-  rectanglesColor: {
-    width: 0,
-    marginTop: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-  },
-  rectangleGray1: {
-    width: 0,
-    fontSize: 0,
-    paddingLeft: 0,
-    paddingTop: 0,
-  },
-  rectangleGray2: {
-    width: 0,
-  },
-  rectangleCian: {
-    width: 0,
-    height: 0,
-  },
-  rectangleBlue: {
-    width: 0,
-    height: 0,
-    marginLeft: 0,
-    marginRight: 0,
-  },
-  rectanglePing: {
-    width: 0,
-    height: 0,
-  },
-  mainConatiner: {
-    width: '100%',
-  }
-};
-
-const getMobileDimensions = (s = stylesZ, orientation) => {
-  const widthFullScreen = [414, 800];
-  s.rectangleGreen = layoutZPhone.getBox(widthFullScreen, [48, 48], orientation);
-  s.rectangleOrange = layoutZPhone.getBox(widthFullScreen, [48, 48], orientation);
-  s.rectangleWhite = layoutZPhone.getBox(widthFullScreen, [80, 80], orientation);
-  s.rectangleWhite.fontSize = layoutZPhone.getFontSize([32, 32], orientation);
-  s.rectangleBigBlack = layoutZPhone.getBox(widthFullScreen, [162, 162], orientation);
-
-  s.rectanglesGray = layoutZPhone.getBox(widthFullScreen, [96, 96], orientation);
-  s.rectanglesGray.marginTop = layoutZPhone.getHeight([48, 48], orientation);
-
-  s.rectangleGray1.width = layoutZPhone.getWidth([207, 400], orientation);
-  s.rectangleGray1.fontSize = layoutZPhone.getFontSize([12, 12], orientation);
-  s.rectangleGray1.paddingTop = layoutZPhone.getHeight([12, 16], orientation);
-  s.rectangleGray1.paddingLeft = layoutZPhone.getWidth([12, 16], orientation);
-
-  s.rectangleGray2.width = layoutZPhone.getWidth([110, 213], orientation);
-
-  s.rectangleDarkGray = layoutZPhone.getBox([222, 429], [72, 72], orientation);
-  s.rectangleDarkGray.marginTop = layoutZPhone.getHeight([84, 84], orientation);
-  s.rectangleDarkGray.marginLeft = layoutZPhone.getWidth([96, 234], orientation);
-  s.rectangleDarkGray.fontSize = layoutZPhone.getFontSize([16, 16], orientation);
-
-  s.rectanglesColor.width = layoutZPhone.getWidth(widthFullScreen, orientation);
-  s.rectanglesColor.marginTop = layoutZPhone.getHeight([56, 56], orientation);
-  s.rectanglesColor.paddingTop = layoutZPhone.getHeight([8, 8], orientation);
-  s.rectanglesColor.paddingBottom = layoutZPhone.getHeight([8, 8], orientation);
-
-  s.rectangleCian = layoutZPhone.getBox([124, 240], [104, 104], orientation);
-
-  s.rectangleBlue = layoutZPhone.getBox([159, 307], [104, 104], orientation);
-  s.rectangleBlue.marginLeft = layoutZPhone.getWidth([12, 16], orientation);
-  s.rectangleBlue.marginRight = layoutZPhone.getWidth([12, 16], orientation);
-
-  s.rectanglePing = layoutZPhone.getBox([87, 168], [104, 104], orientation);
-  s.mainConatiner.width = layoutZPhone.getWidth(widthFullScreen, orientation);
-  return s;
-};
-const getDesktopDimensions = (s = stylesZ, orientation) => {
-  const widthFullScreen = [1440, 1440];
-  s.rectangleGreen = layoutZDesktop.getBox(widthFullScreen, [48, 48], orientation);
-  s.rectangleOrange = layoutZDesktop.getBox(widthFullScreen, [48, 48], orientation);
-  s.rectangleWhite = layoutZDesktop.getBox(widthFullScreen, [80, 80], orientation);
-  s.rectangleWhite.fontSize = layoutZDesktop.getFontSize([32, 32], orientation);
-  s.rectangleBigBlack = layoutZDesktop.getBox(widthFullScreen, [96, 96], orientation);
-
-  s.rectanglesGray = layoutZDesktop.getBox([1280, 1280], [96, 96], orientation, true);
-  s.rectanglesGray.marginTop = layoutZDesktop.getHeight([110, 110], orientation, true);
-  s.rectanglesGray.marginLeft = layoutZDesktop.getWidth([80, 80], orientation, true);
-
-  s.rectangleGray1.width = layoutZDesktop.getWidth([207, 207], orientation, true);
-  s.rectangleGray1.fontSize = layoutZDesktop.getFontSize([14, 14], orientation, true);
-  s.rectangleGray1.paddingTop = layoutZDesktop.getHeight([16, 16], orientation, true);
-  s.rectangleGray1.paddingLeft = layoutZDesktop.getWidth([16, 16], orientation, true);
-
-  s.rectangleGray2.width = layoutZDesktop.getWidth([155, 155], orientation, true);
-
-  s.rectangleDarkGray = layoutZDesktop.getBox([227, 227], [64, 64], orientation, true);
-  s.rectangleDarkGray.marginTop = layoutZDesktop.getHeight([196, 196], orientation);
-  s.rectangleDarkGray.marginLeft = layoutZDesktop.getWidth([606, 606], orientation, true);
-  s.rectangleDarkGray.fontSize = layoutZDesktop.getFontSize([16, 16], orientation, true);
-
-  s.rectanglesColor.width = layoutZDesktop.getWidth(widthFullScreen, orientation);
-  s.rectanglesColor.marginTop = layoutZDesktop.getHeight([118, 118], orientation);
-  s.rectanglesColor.paddingTop = layoutZDesktop.getHeight([24, 24], orientation);
-  s.rectanglesColor.paddingBottom = layoutZDesktop.getHeight([40, 40], orientation);
-
-  s.rectangleCian = layoutZDesktop.getBox([407, 407], [104, 104], orientation, true);
-
-  s.rectangleBlue = layoutZDesktop.getBox([540, 540], [104, 104], orientation, true);
-  s.rectangleBlue.marginLeft = layoutZDesktop.getWidth([32, 32], orientation, true);
-  s.rectangleBlue.marginRight = layoutZDesktop.getWidth([32, 32], orientation, true);
-
-  s.rectanglePing = layoutZDesktop.getBox([269, 269], [104, 104], orientation, true);
-  s.mainConatiner.width = layoutZDesktop.getWidth(widthFullScreen, orientation, false);
-  return s;
-};
-const getTabletDimensions = (s = stylesZ, orientation) => {
-  const widthFullScreen = [768, 1024];
-  s.rectangleGreen = layoutZTablet.getBox(widthFullScreen, [48, 48], orientation);
-  s.rectangleOrange = layoutZTablet.getBox(widthFullScreen, [48, 48], orientation);
-  s.rectangleWhite = layoutZTablet.getBox(widthFullScreen, [80, 80], orientation);
-  s.rectangleWhite.fontSize = layoutZTablet.getFontSize([32, 32], orientation);
-  s.rectangleBigBlack = layoutZTablet.getBox(widthFullScreen, [88, 88], orientation);
-
-  s.rectanglesGray = layoutZTablet.getBox([720, 912], [86, 86], orientation);
-  s.rectanglesGray.marginTop = layoutZTablet.getHeight([80, 76], orientation);
-  s.rectanglesGray.marginLeft = layoutZTablet.getWidth([24, 56], orientation);
-
-  s.rectangleGray1.width = layoutZTablet.getWidth([147, 147], orientation);
-  s.rectangleGray1.fontSize = layoutZTablet.getFontSize([14, 14], orientation);
-  s.rectangleGray1.paddingTop = layoutZTablet.getHeight([16, 16], orientation);
-  s.rectangleGray1.paddingLeft = layoutZTablet.getWidth([8, 8], orientation);
-
-  s.rectangleGray2.width = layoutZTablet.getWidth([110, 110], orientation);
-
-  s.rectangleDarkGray = layoutZTablet.getBox([195, 195], [64, 64], orientation);
-  s.rectangleDarkGray.marginTop = layoutZTablet.getHeight([288, 78], orientation);
-  s.rectangleDarkGray.marginLeft = layoutZTablet.getWidth([287, 415], orientation);
-  s.rectangleDarkGray.fontSize = layoutZTablet.getFontSize([16, 16], orientation);
-
-  s.rectanglesColor.width = layoutZTablet.getWidth(widthFullScreen, orientation);
-  s.rectanglesColor.marginTop = layoutZTablet.getHeight([92, 50], orientation);
-  s.rectanglesColor.paddingTop = layoutZTablet.getHeight([16, 16], orientation);
-  s.rectanglesColor.paddingBottom = layoutZTablet.getHeight([41, 41], orientation);
-
-  s.rectangleCian = layoutZTablet.getBox([224, 289], [93, 93], orientation);
-
-  s.rectangleBlue = layoutZTablet.getBox([298, 384], [93, 93], orientation);
-  s.rectangleBlue.marginLeft = layoutZTablet.getWidth([24, 24], orientation);
-  s.rectangleBlue.marginRight = layoutZTablet.getWidth([24, 24], orientation);
-
-  s.rectanglePing = layoutZTablet.getBox([150, 191], [93, 93], orientation);
-  s.mainConatiner.width = layoutZTablet.getWidth(widthFullScreen, orientation);
-  return s;
-};
+import {
+  convertedStyles as layoutConvertedStyles,
+  defaultStyles as defaultLayoutStyles,
+} from '../styles/layoutScreenStyles';
 
 const LayoutScreenConverter = ({
   orientation = 'portrait',
   deviceType = 'desktop',
   resize = 0,
 }) => {
-  const dimensions = useMemo(() => {
+  const layoutStyles = useMemo(() => {
     // console.log(deviceType, orientation);
-    let dimensions = stylesZ;
+    let styles = Object.assign({}, defaultLayoutStyles);
     switch (deviceType) {
       case 'mobile':
-        dimensions = getMobileDimensions(stylesZ, orientation);
+        styles = layoutConvertedStyles.getMobile(orientation);
         break;
       case 'desktop':
-        dimensions = getDesktopDimensions(stylesZ, orientation);
+        styles = layoutConvertedStyles.getDesktop(orientation);
         break;
       case 'tablet':
-        dimensions = getTabletDimensions(stylesZ, orientation);
+        styles = layoutConvertedStyles.getTablet(orientation);
         break;
       default:
-        dimensions = getMobileDimensions(stylesZ, orientation);
+        styles = layoutConvertedStyles.getDesktop(orientation);
         break;
     }
-    return dimensions;
+    return styles;
   }, [orientation, resize, deviceType]);
 
   const rectangleGreen = () => {
     console.log(orientation);
     return (
       <div style={{
-        width: dimensions.rectangleGreen.width,
-        height: dimensions.rectangleGreen.height,
+        width: layoutStyles.rectangleGreen.width,
+        height: layoutStyles.rectangleGreen.height,
         backgroundColor: '#49c400',
       }}
       />
@@ -243,8 +48,8 @@ const LayoutScreenConverter = ({
   const rectangleOrange = () => {
     return (
       <div style={{
-        width: dimensions.rectangleOrange.width,
-        height: dimensions.rectangleOrange.height,
+        width: layoutStyles.rectangleOrange.width,
+        height: layoutStyles.rectangleOrange.height,
         backgroundColor: '#ff9c24',
       }}
       />
@@ -254,14 +59,14 @@ const LayoutScreenConverter = ({
   const rectangleWhite = () => {
     return (
       <div style={{
-        width: dimensions.rectangleWhite.width,
-        height: dimensions.rectangleWhite.height,
+        width: layoutStyles.rectangleWhite.width,
+        height: layoutStyles.rectangleWhite.height,
         backgroundColor: '#ffffff',
         display: 'table',
       }}
       >
         <div style={{
-          fontSize: dimensions.rectangleWhite.fontSize,
+          fontSize: layoutStyles.rectangleWhite.fontSize,
           color: 'black',
           display: 'table-cell',
           verticalAlign: 'middle',
@@ -277,9 +82,9 @@ const LayoutScreenConverter = ({
   const rectangleBigBlack = () => {
     return (
       <div style={{
-        width: dimensions.rectangleBigBlack.width,
-        height: dimensions.rectangleBigBlack.height,
-        marginBottom: dimensions.rectanglesGray.marginTop,
+        width: layoutStyles.rectangleBigBlack.width,
+        height: layoutStyles.rectangleBigBlack.height,
+        marginBottom: layoutStyles.rectangleBigBlack.marginBottom,
         backgroundColor: '#000',
       }}
       />
@@ -288,8 +93,8 @@ const LayoutScreenConverter = ({
 
   const rectanglesGray = () => {
     return (<div style={{
-      width: dimensions.rectanglesGray.width,
-      height: dimensions.rectanglesGray.height,
+      width: layoutStyles.rectanglesGray.width,
+      height: layoutStyles.rectanglesGray.height,
       position: 'relative',
       margin: 'auto',
     }}
@@ -302,10 +107,10 @@ const LayoutScreenConverter = ({
   const rectangleGray1 = () => {
     return (
       <div style={{
-        width: dimensions.rectangleGray1.width,
-        fontSize: dimensions.rectangleGray1.fontSize,
-        paddingLeft: dimensions.rectangleGray1.paddingLeft,
-        paddingTop: dimensions.rectangleGray1.paddingTop,
+        width: layoutStyles.rectangleGray1.width,
+        fontSize: layoutStyles.rectangleGray1.fontSize,
+        paddingLeft: layoutStyles.rectangleGray1.paddingLeft,
+        paddingTop: layoutStyles.rectangleGray1.paddingTop,
         height: '100%',
         backgroundColor: '#959595',
         float: 'left',
@@ -329,7 +134,7 @@ const LayoutScreenConverter = ({
   const rectangleGray2 = () => {
     return (
       <div style={{
-        width: dimensions.rectangleGray2.width,
+        width: layoutStyles.rectangleGray2.width,
         height: '100%',
         backgroundColor: '#cccccc',
         float: 'right',
@@ -341,16 +146,16 @@ const LayoutScreenConverter = ({
   const rectangleDarkGray = () => {
     return (
       <div style={{
-        width: dimensions.rectangleDarkGray.width,
-        height: dimensions.rectangleDarkGray.height,
-        margin: `${dimensions.rectangleDarkGray.marginTop}px auto 0px auto`,
+        width: layoutStyles.rectangleDarkGray.width,
+        height: layoutStyles.rectangleDarkGray.height,
+        margin: `${layoutStyles.rectangleDarkGray.marginTop}px auto 0px auto`,
         backgroundColor: '#383838',
         position: 'relative',
         display: 'table',
       }}
       >
         <div style={{
-          fontSize: dimensions.rectangleDarkGray.fontSize,
+          fontSize: layoutStyles.rectangleDarkGray.fontSize,
           color: '#ffffff',
           display: 'table-cell',
           verticalAlign: 'middle',
@@ -365,10 +170,10 @@ const LayoutScreenConverter = ({
 
   const rectanglesColor = () => {
     return (<div style={{
-      width: dimensions.rectanglesColor.width,
-      marginTop: dimensions.rectanglesColor.marginTop,
-      paddingTop: dimensions.rectanglesColor.paddingTop,
-      paddingBottom: dimensions.rectanglesColor.paddingBottom,
+      width: layoutStyles.rectanglesColor.width,
+      marginTop: layoutStyles.rectanglesColor.marginTop,
+      paddingTop: layoutStyles.rectanglesColor.paddingTop,
+      paddingBottom: layoutStyles.rectanglesColor.paddingBottom,
       display: 'flex',
       backgroundColor: '#afafaf',
       justifyContent: 'center',
@@ -383,8 +188,8 @@ const LayoutScreenConverter = ({
   const rectangleCian = () => {
     return (
       <div style={{
-        width: dimensions.rectangleCian.width,
-        height: dimensions.rectangleCian.height,
+        width: layoutStyles.rectangleCian.width,
+        height: layoutStyles.rectangleCian.height,
         backgroundColor: '#55ecce',
       }}
       />
@@ -394,10 +199,10 @@ const LayoutScreenConverter = ({
   const rectangleBlue = () => {
     return (
       <div style={{
-        width: dimensions.rectangleBlue.width,
-        height: dimensions.rectangleBlue.height,
-        marginLeft: dimensions.rectangleBlue.marginLeft,
-        marginRight: dimensions.rectangleBlue.marginRight,
+        width: layoutStyles.rectangleBlue.width,
+        height: layoutStyles.rectangleBlue.height,
+        marginLeft: layoutStyles.rectangleBlue.marginLeft,
+        marginRight: layoutStyles.rectangleBlue.marginRight,
         backgroundColor: '#556eec',
       }}
       />
@@ -407,8 +212,8 @@ const LayoutScreenConverter = ({
   const rectanglePing = () => {
     return (
       <div style={{
-        width: dimensions.rectanglePing.width,
-        height: dimensions.rectanglePing.height,
+        width: layoutStyles.rectanglePing.width,
+        height: layoutStyles.rectanglePing.height,
         backgroundColor: '#cc55ec',
       }}
       />
@@ -419,7 +224,7 @@ const LayoutScreenConverter = ({
     <div style={{
       backgroundColor: 'white',
       margin: 'auto',
-      width: dimensions.mainConatiner.width,
+      width: layoutStyles.mainConatiner.width,
     }}>
       {rectangleGreen()}
       {rectangleOrange()}
---
+++ src/styles/layoutScreenStyles.js
@@ -0,0 +1,204 @@
+import { LayoutZeplin, devicesDimensions } from '../helpers/LayoutZeplinConverter';
+/*
+  Mobile
+  https://app.zeplin.io/project/61a8a9480bf3cf8df5b66ab2/screen/61b70b84075a1e48410f5f88
+*/
+const layoutZPhone = new LayoutZeplin(devicesDimensions.iphone11_1111);
+/*
+  Desktop
+  https://app.zeplin.io/project/61a8a9480bf3cf8df5b66ab2/screen/620a368451206fa8e5757617
+*/
+const layoutZDesktop = new LayoutZeplin(devicesDimensions.desktop_1440);
+
+/*
+  Tablet
+  https://app.zeplin.io/project/61a8a9480bf3cf8df5b66ab2/screen/620a3678a46278aabb5de991
+*/
+const layoutZTablet = new LayoutZeplin(devicesDimensions.ipad_1111);
+
+const defaultStyles = {
+  rectangleGreen: {
+    width: 0,
+    height: 0,
+  },
+  rectangleOrange: {
+    width: 0,
+    height: 0,
+  },
+  rectangleWhite: {
+    width: 0,
+    height: 0,
+    fontSize: 0,
+  },
+  rectangleBigBlack: {
+    width: 0,
+    height: 0,
+    marginBottom: 0,
+  },
+  rectanglesGray: {
+    width: 0,
+    height: 0,
+    marginTop: 0,
+    marginLeft: 0,
+  },
+  rectangleDarkGray: {
+    width: 0,
+    height: 0,
+    marginTop: 0,
+    fontSize: 0,
+  },
+  rectanglesColor: {
+    width: 0,
+    marginTop: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
+  rectangleGray1: {
+    width: 0,
+    fontSize: 0,
+    paddingLeft: 0,
+    paddingTop: 0,
+  },
+  rectangleGray2: {
+    width: 0,
+  },
+  rectangleCian: {
+    width: 0,
+    height: 0,
+  },
+  rectangleBlue: {
+    width: 0,
+    height: 0,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  rectanglePing: {
+    width: 0,
+    height: 0,
+  },
+  mainConatiner: {
+    width: '100%',
+  }
+};
+
+const convertedStyles = {
+  getMobile: (orientation) => {
+    const widthFullScreen = [414, 800];
+    const layout = layoutZPhone;
+    const s = Object.assign({}, defaultStyles);
+    s.rectangleGreen = layout.getBox(widthFullScreen, [48, 48], orientation);
+    s.rectangleOrange = layout.getBox(widthFullScreen, [48, 48], orientation);
+    s.rectangleWhite = layout.getBox(widthFullScreen, [80, 80], orientation);
+    s.rectangleWhite.fontSize = layout.getFontSize([32, 32], orientation);
+    s.rectangleBigBlack = layout.getBox(widthFullScreen, [162, 162], orientation);
+    s.rectangleBigBlack.marginBottom = layout.getHeight([48, 48], orientation);
+  
+    s.rectanglesGray = layout.getBox(widthFullScreen, [96, 96], orientation);
+  
+    s.rectangleGray1.width = layout.getWidth([207, 400], orientation);
+    s.rectangleGray1.fontSize = layout.getFontSize([12, 12], orientation);
+    s.rectangleGray1.paddingTop = layout.getHeight([12, 16], orientation);
+    s.rectangleGray1.paddingLeft = layout.getWidth([12, 16], orientation);
+  
+    s.rectangleGray2.width = layout.getWidth([110, 213], orientation);
+  
+    s.rectangleDarkGray = layout.getBox([222, 429], [72, 72], orientation);
+    s.rectangleDarkGray.marginTop = layout.getHeight([84, 84], orientation);
+    s.rectangleDarkGray.fontSize = layout.getFontSize([16, 16], orientation);
+  
+    s.rectanglesColor.width = layout.getWidth(widthFullScreen, orientation);
+    s.rectanglesColor.marginTop = layout.getHeight([56, 56], orientation);
+    s.rectanglesColor.paddingTop = layout.getHeight([8, 8], orientation);
+    s.rectanglesColor.paddingBottom = layout.getHeight([8, 8], orientation);
+  
+    s.rectangleCian = layout.getBox([124, 240], [104, 104], orientation);
+  
+    s.rectangleBlue = layout.getBox([159, 307], [104, 104], orientation);
+    s.rectangleBlue.marginLeft = layout.getWidth([12, 16], orientation);
+    s.rectangleBlue.marginRight = layout.getWidth([12, 16], orientation);
+  
+    s.rectanglePing = layout.getBox([87, 168], [104, 104], orientation);
+    s.mainConatiner.width = layout.getWidth(widthFullScreen, orientation);
+    return s;
+  },
+  getDesktop: (orientation) => {
+    const widthFullScreen = [1440, 1440];
+    const layout = layoutZDesktop;
+    const s = Object.assign({}, defaultStyles);
+    s.rectangleGreen = layout.getBox(widthFullScreen, [48, 48], orientation);
+    s.rectangleOrange = layout.getBox(widthFullScreen, [48, 48], orientation);
+    s.rectangleWhite = layout.getBox(widthFullScreen, [80, 80], orientation);
+    s.rectangleWhite.fontSize = layout.getFontSize([32, 32], orientation, true);
+    s.rectangleBigBlack = layout.getBox(widthFullScreen, [96, 96], orientation);
+    s.rectangleBigBlack.marginBottom = layout.getHeight([110, 110], orientation);
+  
+    s.rectanglesGray = layout.getBox([1280, 1280], [96, 96], orientation, true);
+  
+    s.rectangleGray1.width = layout.getWidth([207, 207], orientation, true);
+    s.rectangleGray1.fontSize = layout.getFontSize([14, 14], orientation, true);
+    s.rectangleGray1.paddingTop = layout.getHeight([16, 16], orientation, true);
+    s.rectangleGray1.paddingLeft = layout.getWidth([16, 16], orientation, true);
+  
+    s.rectangleGray2.width = layout.getWidth([155, 155], orientation, true);
+  
+    s.rectangleDarkGray = layout.getBox([227, 227], [64, 64], orientation, true);
+    s.rectangleDarkGray.marginTop = layout.getHeight([196, 196], orientation);
+    s.rectangleDarkGray.fontSize = layout.getFontSize([16, 16], orientation, true);
+  
+    s.rectanglesColor.width = layout.getWidth(widthFullScreen, orientation);
+    s.rectanglesColor.marginTop = layout.getHeight([118, 118], orientation);
+    s.rectanglesColor.paddingTop = layout.getHeight([24, 24], orientation);
+    s.rectanglesColor.paddingBottom = layout.getHeight([40, 40], orientation);
+  
+    s.rectangleCian = layout.getBox([407, 407], [104, 104], orientation, true);
+  
+    s.rectangleBlue = layout.getBox([540, 540], [104, 104], orientation, true);
+    s.rectangleBlue.marginLeft = layout.getWidth([32, 32], orientation, true);
+    s.rectangleBlue.marginRight = layout.getWidth([32, 32], orientation, true);
+  
+    s.rectanglePing = layout.getBox([269, 269], [104, 104], orientation, true);
+    s.mainConatiner.width = layout.getWidth(widthFullScreen, orientation);
+    return s;
+  },
+  getTablet: (orientation) => {
+    const widthFullScreen = [768, 1024];
+    const layout = layoutZTablet;
+    const s = Object.assign({}, defaultStyles);
+    s.rectangleGreen = layout.getBox(widthFullScreen, [48, 48], orientation);
+    s.rectangleOrange = layout.getBox(widthFullScreen, [48, 48], orientation);
+    s.rectangleWhite = layout.getBox(widthFullScreen, [80, 80], orientation);
+    s.rectangleWhite.fontSize = layout.getFontSize([32, 32], orientation);
+    s.rectangleBigBlack = layout.getBox(widthFullScreen, [88, 88], orientation);
+    s.rectangleBigBlack.marginBottom = layout.getHeight([80, 76], orientation);
+  
+    s.rectanglesGray = layout.getBox([720, 912], [86, 86], orientation);
+  
+    s.rectangleGray1.width = layout.getWidth([147, 147], orientation);
+    s.rectangleGray1.fontSize = layout.getFontSize([14, 14], orientation);
+    s.rectangleGray1.paddingTop = layout.getHeight([16, 16], orientation);
+    s.rectangleGray1.paddingLeft = layout.getWidth([8, 8], orientation);
+  
+    s.rectangleGray2.width = layout.getWidth([110, 110], orientation);
+  
+    s.rectangleDarkGray = layout.getBox([195, 195], [64, 64], orientation);
+    s.rectangleDarkGray.marginTop = layout.getHeight([288, 78], orientation);
+    s.rectangleDarkGray.fontSize = layout.getFontSize([16, 16], orientation);
+  
+    s.rectanglesColor.width = layout.getWidth(widthFullScreen, orientation);
+    s.rectanglesColor.marginTop = layout.getHeight([92, 50], orientation);
+    s.rectanglesColor.paddingTop = layout.getHeight([16, 16], orientation);
+    s.rectanglesColor.paddingBottom = layout.getHeight([41, 41], orientation);
+  
+    s.rectangleCian = layout.getBox([224, 289], [93, 93], orientation);
+  
+    s.rectangleBlue = layout.getBox([298, 384], [93, 93], orientation);
+    s.rectangleBlue.marginLeft = layout.getWidth([24, 24], orientation);
+    s.rectangleBlue.marginRight = layout.getWidth([24, 24], orientation);
+  
+    s.rectanglePing = layout.getBox([150, 191], [93, 93], orientation);
+    s.mainConatiner.width = layout.getWidth(widthFullScreen, orientation);
+    return s;
+  },
+}
+
+export { convertedStyles, defaultStyles };
